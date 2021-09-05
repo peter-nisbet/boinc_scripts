23619937c3193b0e1e31ba4de0b9ab54dc12675e
Peter Nisbet <peter@Peters-iMac.local>
2021-09-05 10:03:12 -0400

    Added delay of 0.1ms to avoid issue where command was getting missed when three + terminal windows were opened.

--- boinc.sh
+++ boinc.sh
@@ -4,7 +4,9 @@ osascript\
 	-e "tell application \"Terminal\" to activate"\
 	-e "tell application \"System Events\" to tell process \"Terminal\" to keystroke \"t\" using command down"\
 	-e "tell application \"Terminal\" to do script \"ssh -t peter@192.168.0.179 'boinctui && uptime && hostname'\" in selected tab of front window"\
+	-e "delay 0.1"\
 	-e "tell application \"System Events\" to tell process \"Terminal\" to keystroke \"t\" using command down"\
 	-e "tell application \"Terminal\" to do script \"ssh -t peter@192.168.0.119 'boinctui && uptime && hostname'\" in selected tab of front window"\
+	-e "delay 0.1"\
 	-e "tell application \"System Events\" to tell process \"Terminal\" to keystroke \"t\" using command down"\
 	-e "tell application \"Terminal\" to do script \"ssh -t peter@192.168.0.142 'boinctui && uptime && hostname'\" in selected tab of front window"
